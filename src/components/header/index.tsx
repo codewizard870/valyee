c69e3f2d648dc3bfe4eda8a897569e91b9567a62
codewizard870 <alenzer0902@gmail.com>
2024-07-11 11:57:26 -0400

    add feedbacks

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,27 +1,30 @@
 import React from "react";
 import { ScrollTrigger, Tween } from "react-gsap";
 import { BsDiscord } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
-type HeaderProps = {
-};
+type HeaderProps = {};
 
-const Header: React.FC<HeaderProps> = ({  }) => {
+const Header: React.FC<HeaderProps> = ({}) => {
+  const navigate = useNavigate();
+
   return (
     <div className="fixed top-0 z-20 flex justify-between w-full px-10 py-6 border-b md:px-20 bg-gray-2 border-b-gray-1/10">
-      <div className="flex items-center gap-2">
+      <div
+        className="flex items-center gap-2 cursor-pointer"
+        onClick={() => navigate("/")}
+      >
         <span className="text-xl font-semibold md:text-2xl">Valyee</span>
       </div>
-      <div className="items-center hidden gap-4 md:flex md:gap-8">
-        {Routes.map((route, index) => (
-          <a
-            href={route.href}
-            key={index}
-          >
-            <span className="text-base md:text-lg">{route.label}</span>
-          </a>
-        ))}
-
-        <button className="justify-center items-center px-[15px] py-[14px] text-base md:text-lg text-white font-medium bg-primary-default hover:bg-primary-default/70 rounded-xl cursor-pointer">
+      <div className="flex gap-4">
+        <div className="items-center hidden gap-4 md:flex md:gap-8">
+          {Routes.map((route, index) => (
+            <a href={route.href} key={index}>
+              <span className="text-sm md:text-base">{route.label}</span>
+            </a>
+          ))}
+        </div>
+        <button className="justify-center items-center px-[15px] py-[14px] text-sm md:text-base text-white font-medium bg-primary-default hover:bg-primary-default/70 rounded-xl cursor-pointer">
           Sign Up for Free!
         </button>
       </div>
@@ -33,16 +36,12 @@ export default Header;
 
 const Routes = [
   {
-    label: "Features",
-    href: "/",
-  },
-  {
     label: "Pricing",
     href: "/pricing",
   },
   {
     label: "Blog",
-    href: "/blog"
+    href: "/blog",
   },
   {
     label: "Log In",
--- src/pages/Home/feedback/index.tsx
+++ src/pages/Home/feedback/index.tsx
@@ -1,28 +1,89 @@
+import Slider from "react-slick";
+// import "../../../assets/scss/slick-slider/slick.min.scss";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const Feedback = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
   return (
-    <div className="mt-[30px] mb-[30px] mx-6 md:mx-[118px] bg-third-default rounded-lg h-[460px] flex justify-center items-center">
-      <div className="w-full md:w-[756px] p-4">
-        <p className="text-xl font-semibold md:text-2xl">
-          “Valyee is a great product! Lorem Ipsum Lorem IpsumLorem IpsumLorem
-          IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem
-          IpsumLorem IpsumLorem IpsumLorem Ipsum
-        </p>
-        <div className="pt-[50px] flex gap-[13px] items-center">
-          <img src="/assets/feedback/avatar.png" className="w-14 md:w-20"/>
-          <div>
-            <p className="text-base md:text-lg text-black/40">Johnny Owens</p>
-            <div className="flex gap-[5px]">
-              {Array(5)
-                .fill(undefined)
-                .map((_, index) => (
-                  <img src="/assets/feedback/star.png" key={index}/>
-                ))}
+    <div className="mt-[30px] mb-[30px] mx-6 md:mx-[100px]">
+      <Slider {...settings}>
+        {Feedbacks.map((feedback, index) => (
+          <div key={index}>
+            <div className="flex items-center justify-center py-4 mx-4 rounded-lg md:mx-2 md:py-20 bg-third-default">
+              <div className="w-full md:w-[756px] p-4">
+                <p className="text-base font-semibold !leading-relaxed md:text-2xl h-[230px] overflow-hidden">
+                  {feedback.description}
+                </p>
+                <div className="pt-[50px] flex gap-[13px] items-center">
+                  <img
+                    src={feedback.avatar}
+                    className="rounded-full w-14 md:w-20"
+                  />
+                  <div>
+                    <p className="text-sm md:text-lg text-black/40 whitespace-nowrap w-[200px] overflow-hidden">
+                      {feedback.name}
+                    </p>
+                    <div className="flex gap-[5px] mt-2">
+                      {Array(feedback.mark)
+                        .fill(undefined)
+                        .map((_, index) => (
+                          <img
+                            src="/assets/feedback/star.png"
+                            key={index}
+                            className="w-4 md:w-5"
+                          />
+                        ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
+        ))}
+      </Slider>
     </div>
   );
 };
 
 export default Feedback;
+
+const Feedbacks = [
+  {
+    description:
+      "I am currently beta testing Valyee - and the product has been  incredibly helpful in reducing the time I spend hiring! With the ever increasing number of applicants for each job post, Valyee has saved us innumerable hours in downloading and manually reviewing CVs.",
+    avatar: "/assets/feedback/avatar.png",
+    mark: 5,
+    name: "Dhruv Tekchandani, Founder",
+  },
+  {
+    description:
+      "Valyee has been a game-changer for our hiring process. The product has significantly reduced the time we spend sifting through CVs and has streamlined our recruitment efforts. With the increasing number of applicants for each job post, Valyee has saved us countless hours. It's an invaluable tool for any HR team!",
+    avatar: "/assets/feedback/1.png",
+    mark: 5,
+    name: "Ava Johnson, HR Manager at BrightFuture Inc.",
+  },
+  {
+    description:
+      "We've been beta testing Valyee, and the impact on our hiring process has been outstanding. The time saved on downloading and manually reviewing CVs is incredible. With more applicants than ever, Valyee has been essential in managing our recruitment efficiently. Highly recommended!",
+    avatar: "/assets/feedback/2.png",
+    mark: 5,
+    name: "Ethan Martinez, Recruitment Lead at TechWave Solutions",
+  },
+  {
+    description:
+      "Using Valyee during our beta testing phase has been incredibly beneficial. It has drastically cut down the time required to handle the influx of job applications. Valyee automates the CV review process, saving us numerous hours. It's a must-have for any organization facing a high volume of applicants.",
+    avatar: "/assets/feedback/3.png",
+    mark: 5,
+    name: "Olivia Smith, Talent Acquisition Specialist at InnovateX",
+  },
+];
